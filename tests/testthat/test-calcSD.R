23fca3a12c6976cc5d84be35f8cf8869ffa3e8b9
Thijs Janzen <janzen@evolbio.mpg.de>
2016-09-18 17:39:36 +0200

    Added skip to test-calcSD.R

--- tests/testthat/test-calcSD.R
+++ tests/testthat/test-calcSD.R
@@ -1,6 +1,7 @@
 context("calcSD")
 
 test_that("calcSD: use", {
+  skip("Under Construction")
   #calcSD <- function(species, abundances, n_plots, n_traits)  {
 
   set.seed(42)
@@ -39,9 +40,30 @@ test_that("calcSD: use", {
     0
   )
 
+  set.seed(666)
+  n_traits <- 3
+  n_plots <- 10
+  num_species <- 10;
+  x <- generate.Artificial.Data(numSpecies = num_species, numTraits = n_traits,
+                                numCommunities = n_plots,
+                                occurence_distribution = 0.5,
+                                average_richness = 10,
+                                SD_richness = 1,
+                                random.Mechanism = FALSE)
+  data_species <- x$traits
+  data_species$trait1 <- 1:10
+  data_species$trait2 <- 1:10
+  data_species$trait3 <- 1:10
 
+  data_abundances <- x$abundances
+  for (i in 2:length(data_abundances[1,])) {
+    data_abundances[1,i] <- 0
+  }
 
+  scaled_species <- scaleSpeciesvalues(data_species,n_traits)
 
+  x <- calcSD(scaled_species,data_abundances, n_plots, n_traits)
+  x
 
 
   species <- scaled_species
